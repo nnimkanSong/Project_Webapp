0256265883d0d6f1e50ad065f6fd233c717a22b6
Komgrit <Komgrit.krit@gmail.com>
2025-10-09 13:30:06 +0700

    API adminfeedback

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const authRoutes = require('./router/auth');
 const profileRoutes = require('./router/profile');
 const adminBookingRoutes = require('./router/admin_history');
 const feedbackRoutes = require("./router/feedback");
+const admin_feedbackRoutes = require("./router/admin_feedback");
 
 app.use(cors({
   origin: process.env.CLIENT_URL || 'http://localhost:5174',
@@ -30,6 +31,7 @@ app.use('/api/profile', profileRoutes);
 app.use("/api/admin/history", require("./router/admin_history"));
 app.use('/uploads', express.static('uploads'));
 app.use("/api/feedback", feedbackRoutes);
+app.use("/api/admin/feedbacks", admin_feedbackRoutes);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
